de85f40fa713dbf8609f24a5b2ca6226b85551d0
digilanimedia <digilanimedia@gmail.com>
2025-09-07 04:06:19 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: [
-      "img.youtube.com", // for embedded YouTube thumbnails
-      "youtube.com/@DigilaniMedia"   // replace with your domain if hosting images
-    ]
-  },
   experimental: {
-    optimizeCss: true // Next.js CSS optimization
-    eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+    appDir: true
   },
-}
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  images: {
+    domains: ['i.ytimg.com'] // add YouTube thumbnails if needed
+  }
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
