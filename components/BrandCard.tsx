aeff362ac3ac6b83883c0afd2fe1866bf564fa56
digilanimedia <digilanimedia@gmail.com>
2025-09-07 22:05:30 -0400

    Update BrandCard.tsx

--- components/BrandCard.tsx
+++ components/BrandCard.tsx
@@ -19,9 +19,7 @@ export function BrandCard({ title, excerpt, link, className = "", ...props }: Br
     >
       <h3 className="text-xl font-bold mb-2">{title}</h3>
       <p className="text-gray-300 mb-4">{excerpt}</p>
-      <a href={link} className="text-[#e49400] font-semibold">
-        Read More
-      </a>
+      <a href={link} className="text-[#e49400] font-semibold">Read More</a>
     </motion.div>
   );
 }
