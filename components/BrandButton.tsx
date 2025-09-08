3c6de2dd249ee894daeaddccbf735d20b85cff38
digilanimedia <digilanimedia@gmail.com>
2025-09-07 22:37:35 -0400

    Update BrandButton.tsx

--- components/BrandButton.tsx
+++ components/BrandButton.tsx
@@ -1,24 +1,22 @@
-// components/BrandButton.tsx
-import { motion } from "framer-motion";
+import { motion, MotionProps } from "framer-motion";
 import React, { ReactNode } from "react";
 
-interface BrandButtonProps {
+type BrandButtonProps = {
   children: ReactNode;
   className?: string;
-  onClick?: () => void;
-}
+} & MotionProps;
 
 export const BrandButton: React.FC<BrandButtonProps> = ({
   children,
   className = "",
-  onClick,
+  ...motionProps
 }) => {
   return (
     <motion.button
       className={className}
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.97 }}
-      onClick={onClick}
+      {...motionProps}
     >
       {children}
     </motion.button>
